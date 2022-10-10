63ec16e7e3b1b1560311d162d6e735cf137bcb12
Josias <kologojosias@gmail.com>
2022-10-10 06:37:44 +0100

    modified module name

--- utube.js
+++ utube.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { requireNativeComponent, Platform, View } from 'react-native';
 
-const UTComponent = Platform.select({
+const YoutubePlayerView = Platform.select({
   ios: View,
-  android: requireNativeComponent('RNUtubeView'),
+  android: requireNativeComponent('YoutubePlayerView'),
 });
 
 export default class UTubePlayerView extends React.Component {
   render() {
     const { width = '100%', height = 200 } = this.props;
-    return <UTComponent {...this.props} style={{ width, height }} />;
+    return <YoutubePlayerView {...this.props} style={{ width, height }} />;
   }
 }
